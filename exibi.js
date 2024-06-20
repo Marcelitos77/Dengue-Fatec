4237afb3bc37d7fb67c1b797f7f615934427ac2f
bonomo06 <pedrohbnm@gmail.com>
2024-06-20 11:04:22 -0300

    arrumando cores tabela

--- exibi.js
+++ exibi.js
@@ -8,10 +8,10 @@ fetch('alertcity.json')
         casos_est = casos_est.reverse();
 
         const getBarColor = valor => {
-            if (valor <= 100) return 'green';
-            if (valor <= 250) return 'yellow';
-            if (valor <= 400) return 'orange';
-            return 'red';
+            if (valor <= 100) return '#f2cbcb';
+            if (valor <= 250) return '#b87474';
+            if (valor <= 400) return '#874949';
+            return '#512626';
         };
         const backgroundColor = casos_est.map(getBarColor);
 
